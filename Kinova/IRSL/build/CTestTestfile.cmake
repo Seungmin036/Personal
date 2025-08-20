21a2d04a25cb3a574ee6a21a9cef93de1a534564
Seungmin036 <ssm1010914@gmail.com>
2025-08-20 17:10:40 +0900

    Kinova Done

--- Kinova/IRSL/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/irsl/catkin_ws/src
-# Build directory: /home/irsl/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("Mujoco-1dof-main")
--- Kinova/IRSL/build/Mujoco-1dof-main/CMakeFiles/lodepng.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/irsl/catkin_ws/devel/lib/liblodepng.a"
-)
